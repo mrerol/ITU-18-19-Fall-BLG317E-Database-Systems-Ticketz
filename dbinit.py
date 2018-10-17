37ed7302cc57ad8144a49b15ae54631285be6553
Abdullah Akgül <abdullahakgul70@gmail.com>
2018-10-17 19:31:41 +0300

    dbinit.py changes

--- dbinit.py
+++ dbinit.py
@@ -19,7 +19,7 @@ def initialize(url):
 
 
 if __name__ == "__main__":
-    url = os.getenv("DATABASE_URL")
+    url = os.getenv("postgres://itucs:itucspw@localhost:32768/itucsdb")
     if url is None:
         print("Usage: DATABASE_URL=url python dbinit.py", file=sys.stderr)
         sys.exit(1)
