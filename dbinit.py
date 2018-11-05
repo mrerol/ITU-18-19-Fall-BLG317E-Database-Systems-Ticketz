91328be72f9464cb01b184c65884b7081e2dd1c5
aportekila <akgula15@itu.edu.tr>
2018-11-05 15:07:48 +0300

    static file location changed demo table created

--- dbinit.py
+++ dbinit.py
@@ -6,8 +6,30 @@ import psycopg2 as dbapi2
 DATABASE_URL = 'postgres://kalcitdkfyeevw:39cdcacf84047dc48c74f58064a25a7406bd3645c95c712b9ba888f28cab791b@ec2-54-243-187-30.compute-1.amazonaws.com:5432/d96hqqveldfnft'
 
 INIT_STATEMENTS = [
-    "CREATE TABLE IF NOT EXISTS DUMMY (NUM INTEGER)",
-    "INSERT INTO DUMMY VALUES (42)",
+    """CREATE TABLE IF NOT EXISTS hotels 
+    (
+    
+        hotel_id INTEGER SERIAL PRIMARY KEY,
+        name VARCHAR (50) NOT NULL,
+        email VARCHAR (50) NOT NULL,
+        description VARCHAR (250) NOT NULL,
+        city VARCHAR (20),
+        address VARCHAR (250) NOT NULL,
+        phone VARCHAR (15) NOT NULL,
+        website VARCHAR (50)
+                
+    )""",
+    """INSERT INTO hotels VALUES (
+                        1,
+                        "deneme",
+                        "deneme@deneme.com",
+                        "demo of description",
+                        "istanbul",
+                        "deneme sokak deneme cadde deneme",
+                        "0321221222",
+                        "dememe.com"
+    
+    )"""
 ]
 
 
