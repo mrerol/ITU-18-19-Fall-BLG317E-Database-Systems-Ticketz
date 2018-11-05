b53407f3e415ee46e09849ea7658bf26be183255
Abdullah Akgül <abdullahakgul70@gmail.com>
2018-11-05 15:38:35 +0300

    try to create database and insert some data on heroku

--- dbinit.py
+++ dbinit.py
@@ -6,10 +6,12 @@ import psycopg2 as dbapi2
 DATABASE_URL = 'postgres://kalcitdkfyeevw:39cdcacf84047dc48c74f58064a25a7406bd3645c95c712b9ba888f28cab791b@ec2-54-243-187-30.compute-1.amazonaws.com:5432/d96hqqveldfnft'
 
 INIT_STATEMENTS = [
+    """DROP TABLE IF EXISTS hotels""",
+
     """CREATE TABLE IF NOT EXISTS hotels 
     (
     
-        hotel_id INTEGER SERIAL PRIMARY KEY,
+        hotel_id SERIAL NOT NULL PRIMARY KEY,
         name VARCHAR (50) NOT NULL,
         email VARCHAR (50) NOT NULL,
         description VARCHAR (250) NOT NULL,
@@ -19,17 +21,78 @@ INIT_STATEMENTS = [
         website VARCHAR (50)
                 
     )""",
+
     """INSERT INTO hotels VALUES (
                         1,
-                        "deneme",
-                        "deneme@deneme.com",
-                        "demo of description",
-                        "istanbul",
-                        "deneme sokak deneme cadde deneme",
-                        "0321221222",
-                        "dememe.com"
+                        'deneme',
+                        'deneme@deneme.com',
+                        'demo of description',
+                        'istanbul',
+                        'deneme sokak deneme cadde deneme',
+                        '0321221222',
+                        'dememe.com'
     
-    )"""
+    )""",
+
+    """INSERT INTO hotels VALUES (
+                            2,
+                            'deneme',
+                            'deneme@deneme.com',
+                            'demo of description',
+                            'istanbul',
+                            'deneme sokak deneme cadde deneme',
+                            '0321221222',
+                            'dememe.com'
+    
+        )""",
+
+    """INSERT INTO hotels VALUES (
+                            3,
+                            'deneme',
+                            'deneme@deneme.com',
+                            'demo of description',
+                            'istanbul',
+                            'deneme sokak deneme cadde deneme',
+                            '0321221222',
+                            'dememe.com'
+    
+        )""",
+
+    """INSERT INTO hotels VALUES (
+                            4,
+                            'deneme',
+                            'deneme@deneme.com',
+                            'demo of description',
+                            'istanbul',
+                            'deneme sokak deneme cadde deneme',
+                            '0321221222',
+                            'dememe.com'
+    
+        )""",
+
+    """INSERT INTO hotels VALUES (
+                            5,
+                            'deneme',
+                            'deneme@deneme.com',
+                            'demo of description',
+                            'istanbul',
+                            'deneme sokak deneme cadde deneme',
+                            '0321221222',
+                            'dememe.com'
+    
+        )""",
+
+    """INSERT INTO hotels VALUES (
+                            6,
+                            'deneme',
+                            'deneme@deneme.com',
+                            'demo of description',
+                            'istanbul',
+                            'deneme sokak deneme cadde deneme',
+                            '0321221222',
+                            'dememe.com'
+    
+        )"""
 ]
 
 
