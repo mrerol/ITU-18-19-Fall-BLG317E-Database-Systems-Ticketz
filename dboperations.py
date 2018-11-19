30b72c7778fddb08e1fe9dd55a69646c656c4d37
Abdullah Akgül <abdullahakgul70@gmail.com>
2018-11-19 20:37:52 +0300

    hotel class and some related query created

---
+++ dboperations.py
@@ -0,0 +1,72 @@
+from tables import Hotel
+import psycopg2 as dbapi2
+import os
+import sys
+
+class Database:
+    def __init__(self):
+        self.hotel = self.Hotel()
+
+    class Hotel:
+        def __init__(self):
+            self.url = os.getenv("DATABASE_URL")
+
+        def add_hotel(self, hotel):
+            with dbapi2.connect(self.url) as connection:
+                cursor = connection.cursor()
+                cursor.execute(
+                    "INSERT INTO hotels (name, email, description, city, address, phone, website) VALUES (%s, %s, %s, %s, %s, %s, %s)",
+                    (hotel.name, hotel.email, hotel.description, hotel.city, hotel.address, hotel.phone, hotel.website))
+                cursor.close()
+
+        def delete_hotel(self, hotel_id):
+            try:
+                connection = dbapi2.connect(self.url)
+                cursor = connection.cursor()
+                cursor.execute("DELETE FROM hotels WHERE hotel_id = %s", (hotel_id,))
+                connection.commit()
+                cursor.close()
+            except (Exception, dbapi2.DatabaseError) as error:
+                print(error)
+            finally:
+                if connection is not None:
+                    connection.close()
+
+        def get_hotel(self, hotel_id):
+            _hotel = None
+            print(hotel_id)
+            print("deneme")
+            try:
+                connection = dbapi2.connect(self.url)
+                cursor = connection.cursor()
+                cursor.execute("SELECT * FROM hotels WHERE hotel_id = %s", (hotel_id,))
+                hotel = cursor.fetchone()
+                if hotel is not None:
+                    _hotel = Hotel(hotel[1], hotel[2], hotel[3], hotel[4], hotel[5], hotel[6], hotel[7])
+                connection.commit()
+                cursor.close()
+            except (Exception, dbapi2.DatabaseError) as error:
+                print(error)
+            finally:
+                if connection is not None:
+                    connection.close()
+            return _hotel
+
+        def get_hotels(self):
+            hotels = []
+            try:
+                connection = dbapi2.connect(self.url)
+                cursor = connection.cursor()
+                cursor.execute("SELECT * FROM hotels;")
+                for hotel in cursor:
+                    _hotel = Hotel(hotel[1], hotel[2], hotel[3], hotel[4], hotel[5], hotel[6], hotel[7])
+                    hotels.append((hotel[0], _hotel))
+                connection.commit()
+                cursor.close()
+            except (Exception, dbapi2.DatabaseError) as error:
+                print(error)
+            finally:
+                if connection is not None:
+                    connection.close()
+
+            return hotels
